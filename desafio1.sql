f36d0fa83b14e0c29e17ac067022f84c3d6aa8d2
CalebeLAR <ca6ele@gmail.com>
2023-02-14 10:15:22 -0400

    desafio 01: pondo FOREIGN KEY na tabela usuarios e criando tabela planos

--- desafio1.sql
+++ desafio1.sql
@@ -3,18 +3,19 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
 CREATE TABLE SpotifyClone.usuarios (
-    usuario_id INT AUTO_INCREMENT,
-    nome_usuario VARCHAR(100) NOT NULL,
+    usuario_id TINYINT AUTO_INCREMENT,
+    nome_usuario VARCHAR(36) NOT NULL,
     idade TINYINT NOT NULL,
-    plano_id TINYINT NOT NULL,
+    plano_id TINYINT NOT NULL, 
     data_assinatura DATETIME,
-    CONSTRAINT PRIMARY KEY (usuario_id)
+    CONSTRAINT PRIMARY KEY (usuario_id),
+	FOREIGN KEY (plano_id) REFERENCES SpotifyClone.planos(plano_id)
 ) engine = InnoDB;
 
 INSERT INTO SpotifyClone.usuarios 
 	(nome_usuario, idade, plano_id, data_assinatura)
 VALUE 
-     ('Barbara Liskov', '82', '1', '2019-10-20'),
+     ('Barbara Liskov', '82', 1, '2019-10-20'),
      ('Robert Cecil Martin', '58', '1', '2017-01-06'),
      ('Ada Lovelace', '37', '2', '2017-12-30'),
      ('Martin Fowler', '46', '2', '2017-01-17'),
@@ -25,11 +26,21 @@ VALUE
      ('Judith Butler', '45', '4', '2020-05-13'),
      ('Jorge Amado', '58', '4', '2017-02-17');
 
--- CREATE TABLE SpotifyClone.planos(
--- 	plano_id ,
--- 	nome_plano,
--- 	valor_plano,
--- ) engine = InnoDB;
+CREATE TABLE SpotifyClone.planos(
+	plano_id TINYINT AUTO_INCREMENT,
+	nome_plano VARCHAR(13) NOT NULL ,
+	valor_plano DECIMAL(3, 2) NOT NULL,
+    CONSTRAINT PRIMARY KEY (plano_id)
+) engine = InnoDB;
+
+INSERT INTO SpotifyClone.planos
+	(nome_plano, valor_plano)
+VALUE 
+	('gratuito', 0),
+	('familiar', 7.99),
+	('universitário', 5.99),
+	('pessoal', 6.99);
+	
 
 
 -- CREATE TABLE SpotifyClone.historico_de_musicas(
